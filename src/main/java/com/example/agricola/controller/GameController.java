94934d1106367cfcd521d127ff1af138e6799d8f
Yunjae Kim <jerryfly1@gmail.com>
2024-05-24 00:03:35 +0900

    GameController 약간수정, 미완성

--- src/main/java/com/example/agricola/controller/GameController.java
+++ src/main/java/com/example/agricola/controller/GameController.java
@@ -11,11 +11,22 @@ import java.util.List;
 @Controller
 public class GameController {
     private List<Player> players;
+    /* Back이 Server, Front가 Cleint */
 
-    @MessageMapping("/room/{roomId}/start")
-    @SendTo("/topic/room/{roomId}")
-    public void gameStart(@DestinationVariable String roomId) {
+
+    // Client는 /room/sendGameStartMessage로 메세지를 보냄
+    @MessageMapping("/room/sendGameStartMessage")
+    //메세지 안에 정보가 들어올때 메세지로 부른 메서드에서도 정보를 처리 가능할까?
+
+    // Client는 session.subscribe("/topic/receiveMessage") 이런식으로 구독을함. Server는 SendTo가써져있는 메서드의
+    //모든 동작들의 결과(리턴값 포함)을 SendTo에 써져있는 구독으로 보내는거임.
+    @SendTo("topic/gameStart")
+    public void gameStart(@DestinationVariable String roomId, SimpMessageHeaderAccessor headerAccessor) {
         //플레이어 목록 초기화
+        String sessionId = headerAccessor.getSessionId();
+        GameController gc = new GameController(); // Player객체 만들때
+        Player player = Player(sessionId, gc, roomId); //플레이어 객체 생성
+        player.initializeResource();
 
 
         // Here, add any logic you need to initialize the game state.
